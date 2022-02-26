e16a800e5fc67f32e5105cdd400e7ebc75da4e40
Miniliang <93837029+Miniliang@users.noreply.github.com>
2022-02-25 16:47:15 -0800

    first version

--- main.py
+++ main.py
@@ -7,15 +7,17 @@ app = Flask(__name__)
 def home_page():
     return render_template('home.html')
 
-@app.route("/login")
-def login():
-    return "<p>Login Here</p>"
-
 
 @app.route('/INSTRUCTION')
 def pv():
-    return render_template(('pv.html'))
+    return render_template(('is.html'))
 
-@app.route('/Download')
+@app.route("/CONTACT US")
+def ca():
+    return render_template(('ca.html'))
+
+@app.route('/DOWNLOAD')
 def dw():
     return render_template(('dw.html'))
+
+
